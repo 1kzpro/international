324331541e059ab1bccd97768c57bc011700dfc8
1kzpro <kazymizam@mail.ru>
2020-05-16 17:40:42 -0500

    doge update

--- Python/doge/student.py
+++ Python/doge/student.py
@@ -1,28 +1,27 @@
-import task1
-filepath = 'Python/data.json'
-data = task1.read_json_file(filepath)
-name = data["name"]
-age = data["age"]
-major = data["major"]
-college = data["college"]
-term = data["term"]
-year = data["year"]
-credits_taken = data["credits_taken"]
-credits_reaining = data["credits_reaining"]
-advisor = data["advisor"]
-gpa = data["gpa"]
-print(name)
-print(age)
-print(age)
-print(major)
-print(college)
-print(term)
-print(year)
-print(credits_taken)
-print(credits_reaining)
-print(advisor)
-print(gpa)
 
-class Student:
-    def __init__(self, filepath):
-        
+import task1
+class student:
+      name = ""
+     age = 0
+     major = ""
+     college = ""
+     term = ""
+     year = 0
+     credits_taken = ""
+     credits_reaining = ""
+     advisor = ""
+     gpa = 0
+
+     def __init__(self, filepath):
+           filepath = 'assignment/data.json'
+          data = task1.read_json_file(filepath)
+         self.name = data["name"]
+         self.age = data["age"]
+         self.major = data["major"]
+          self.college = data["college"]
+         self.term = data["term"]
+          self.year = data["year"]
+         self.credits_taken = data["credits_taken"]
+         self.credits_reaining = data["credits_reaining"]
+         self.advisor = data["advisor"]
+          self.gpa = data["gpa"]
