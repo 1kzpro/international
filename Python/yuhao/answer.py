946b294d319c29bd5d7cb49dd8e28f88d6e2adb0
ZYH-01 <55114062+ZYH-01@users.noreply.github.com>
2020-05-16 17:20:53 -0500

    yuhao

---
+++ Python/yuhao/answer.py
@@ -0,0 +1,19 @@
+import task1
+
+filepath = 'Python/yuhao/data.json'
+
+class Student:
+    def __init__(self, filepath):
+        self.data1 = filepath
+
+    def print_all(self, name, age, college, term, year, credits_taken, credits_reaining, advisor, gpa):
+        self.name = name
+        self.age = age
+        self.college = college
+        self.term = term
+        self.year = year
+        self.credits_taken = credits_taken
+        self.credits_reaining = credits_reaining
+        self.advisor = advisor
+        self.gpa = gpa
+        print(self.data1, self.name, self.age, self.college, self.term, self.year, self.credits_taken, self.credits_reaining, self.advisor, self.gpa)
