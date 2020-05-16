031ffb224123031955e8819960fcb5ac4ad4c6af
doge555 <60857941+doge555@users.noreply.github.com>
2020-05-16 17:16:52 -0500

    Create student.py
    answer

---
+++ Python/doge/student.py
@@ -0,0 +1,24 @@
+import task1
+filepath = 'assignment/data.json'
+data = task1.read_json_file(filepath)
+name = data["name"]
+age = data["age"]
+major = data["major"]
+college = data["college"]
+term = data["term"]
+year = data["year"]
+credits_taken = data["credits_taken"]
+credits_reaining = data["credits_reaining"]
+advisor = data["advisor"]
+gpa = data["gpa"]
+print(name)
+print(age)
+print(age)
+print(major)
+print(college)
+print(term)
+print(year)
+print(credts_taken)
+print(credits_reaining)
+print(advisor)
+print(gpa)
