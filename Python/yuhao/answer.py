d1e8b85803753e5b9c8e04b4ab26006afb9a3f91
1kzpro <kazymizam@mail.ru>
2020-05-16 18:06:59 -0500

    yuhao update

--- Python/yuhao/answer.py
+++ Python/yuhao/answer.py
@@ -1,10 +1,17 @@
-import task1
+import task1
 
-filepath = 'Python/yuhao/data.json'
-
-class Student:
-    def __init__(self, filepath):
-        self.data1 = filepath
+class Student:
+    def __init__(self, filepath):
+        data = task1.read_json_file(filepath)
+        self.name = data['name']
+        self.age = data['age']
+        self.college = data['college']
+        self.term = data['term']
+        self.year = data['year']
+        self.credits_taken = data['credits_taken']
+        self.credits_reaining = data['credits_reaining']
+        self.advisor = data['advisor']
+        self.gpa = data['gpa']
 
     def print_all(self, name, age, college, term, year, credits_taken, credits_reaining, advisor, gpa):
         self.name = name
@@ -17,8 +24,3 @@ class Student:
         self.advisor = advisor
         self.gpa = gpa
         print(self.data1, self.name, self.age, self.college, self.term, self.year, self.credits_taken, self.credits_reaining, self.advisor, self.gpa)
-
-
-student = Student(filepath)
-
-print(student.term)
