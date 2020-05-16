8c994593e6e7d9518f19106f0fc582ed3c8c8d71
1kzpro <kazymizam@mail.ru>
2020-05-16 18:04:47 -0500

    new uploads

--- Python/doge/student.py
+++ Python/doge/student.py
@@ -1,27 +1,28 @@
+import doge.task1 as task1
 
-import task1
-class student:
-      name = ""
-     age = 0
-     major = ""
-     college = ""
-     term = ""
-     year = 0
-     credits_taken = ""
-     credits_reaining = ""
-     advisor = ""
-     gpa = 0
+class Student:
+    # name = ""
+    # age = 0
+    # major = ""
+    # college = ""
+    # term = ""
+    # year = 0
+    # credits_taken = ""
+    # credits_reaining = ""
+    # advisor = ""
+    # gpa = 0
 
-     def __init__(self, filepath):
-           filepath = 'assignment/data.json'
-          data = task1.read_json_file(filepath)
-         self.name = data["name"]
-         self.age = data["age"]
-         self.major = data["major"]
-          self.college = data["college"]
-         self.term = data["term"]
-          self.year = data["year"]
-         self.credits_taken = data["credits_taken"]
-         self.credits_reaining = data["credits_reaining"]
-         self.advisor = data["advisor"]
-          self.gpa = data["gpa"]
+    def __init__(self, filepath):
+        # filepath = 'assignment/data.json'
+        data = task1.read_json_file(filepath)
+        self.name = data["name"]
+        self.age = data["age"]
+        self.major = data["major"]
+        self.college = data["college"]
+        self.term = data["term"]
+        self.year = data["year"]
+        self.credits_taken = data["credits_taken"]
+        self.credits_reaining = data["credits_reaining"]
+        self.advisor = data["advisor"]
+        self.gpa = data["gpa"]
+        self.levels = ['FRESHMAN', 'SOPHOMORE', 'JUNIOR', 'SENIOR'] # Class constants and so on
--- Python/second.py
+++ Python/second.py
@@ -1,6 +1,6 @@
 import task1
 
-filepath = 'Python/example.json'
+filepath = 'Python/data.json'
 
 # Example how to get data: data = task1.read_json_file(filepath)
 
@@ -8,6 +8,24 @@ filepath = 'Python/example.json'
 
 # Create class that takes filepath as an argument then creates all instance variables.
 
-# Example how to make instance varibale name = data['name']
+# Example how to make instance varibale 
+
+self.name = data['name']
 
 print(name)
+
+# How to make class with multiple inheritance
+
+class Engine:
+    def __init__(self, engine_type):
+        self.engine = engine_type
+
+class Wheels:
+    def __init__(self, count):
+        self.count = count
+
+class Car(Engine, Wheels):
+    def __init__(self, name, engine_type, count):
+        Engine.__init__(engine_type)
+        Wheels.__init__(count)
+        sefl.name = name
