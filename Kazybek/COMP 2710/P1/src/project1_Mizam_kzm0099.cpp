bda1fee76a167d8a2c6abd5120671d04e13e5d72
1kzpro <kazymizam@mail.ru>
2020-08-25 14:21:58 -0500

    finished working project

--- Kazybek/COMP 2710/P1/src/project1_Mizam_kzm0099.cpp
+++ Kazybek/COMP 2710/P1/src/project1_Mizam_kzm0099.cpp
@@ -5,16 +5,22 @@
 
     Documentation for Pointers: https://www.tutorialspoint.com/cprogramming/c_pointers.htm
     Documentation for Prototype: http://www.cplusplus.com/articles/yAqpX9L8/
+    Documentation for Formatting: https://thispointer.com/c-convert-double-to-string-and-manage-precision-scientific-notation/
 */
 
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include <sstream>
 
-void input(double*, float*, double*);
-int calculate(double*, float*, double*, std::string*, int*, double*);
-void output(std::string*, int*, double*);
+void input(double *, float *, double *);
+int calculate(double *, float *, double *, std::string *, int *, double *);
+void output(std::string *, int *, double *);
+std::string format_precision_2(double);
+std::string format_precision_2(float);
 
-int main() {
+int main()
+{
     // Declaring input variables
     double loanAmount;
     float interestRate;
@@ -29,53 +35,63 @@ int main() {
 
     short int status = calculate(&loanAmount, &interestRate, &monthlyPayment, &calculatedOutput, &months, &totalInterest);
 
-    if (status == 0) {
+    if (status == 0)
+    {
         output(&calculatedOutput, &months, &totalInterest);
     }
-    else {
-        std::cout << "I am sorry but you are poor(";
+    else
+    {
+        std::cout << "Your monthly payment is not enough to pay loan.";
     }
 
     return 0;
-
 }
 
-void input(double *loanAmount, float *interestRate, double *monthlyPayment) {
+void input(double *loanAmount, float *interestRate, double *monthlyPayment)
+{
     std::cout << "Loan Amount: ";
     std::cin >> *loanAmount;
-    std::cout << "Interest Rate(% per year): " ;
+    std::cout << "Interest Rate(% per year): ";
     std::cin >> *interestRate;
     std::cout << "Monthly Payments: ";
     std::cin >> *monthlyPayment;
 }
 
-int calculate(double *loanAmount, float *interestRate, double *monthlyPayment, std::string *calculatedOutput, int* months, double* totalInterest) {
+int calculate(double *loanAmount, float *interestRate, double *monthlyPayment, std::string *calculatedOutput, int *months, double *totalInterest)
+{
     double balance = *loanAmount;
     float monthlyInterestRate = *interestRate / 12;
 
-    for (int month = 0; balance > 0.00; month++) {
+    for (int month = 0; balance > 0.00; month++)
+    {
         double interest = balance * monthlyInterestRate / 100;
         double principial = *monthlyPayment - interest;
         double payment = interest + principial;
 
-        if (principial < 0) {
+        if (principial < 0)
+        {
             return 1;
         }
 
-        if (month == 0) {
-            *calculatedOutput += std::to_string(month) + "\t$" + std::to_string(balance) + "\tN/A\tN/A\tN/A\tN/A\n";
+        if (month == 0)
+        {
+            *calculatedOutput += std::to_string(month) + "\t$" + format_precision_2(balance) + "\tN/A\tN/A\tN/A\tN/A\n";
             continue;
         }
 
-        if (balance < principial) {
+        if (balance < principial)
+        {
             principial = balance;
         }
 
         balance -= principial;
 
-        *calculatedOutput += std::to_string(month) + "\t$" + std::to_string(balance) + "\t$" + std::to_string(payment) 
-            + "\t$" + std::to_string(monthlyInterestRate) + "\t$" + std::to_string(interest) + "\t$" + std::to_string(principial) + "\n";
-
+        // Formatting issue solving
+        if (balance < 1000) {
+            *calculatedOutput += std::to_string(month) + "\t$" + format_precision_2(balance) + "\t\t$" + format_precision_2(payment) + "\t$" + format_precision_2(monthlyInterestRate) + "\t$" + format_precision_2(interest) + "\t$" + format_precision_2(principial) + "\n";
+        } else {
+            *calculatedOutput += std::to_string(month) + "\t$" + format_precision_2(balance) + "\t$" + format_precision_2(payment) + "\t$" + format_precision_2(monthlyInterestRate) + "\t$" + format_precision_2(interest) + "\t$" + format_precision_2(principial) + "\n";
+        }
         (*months)++;
         *totalInterest += interest;
     }
@@ -83,14 +99,43 @@ int calculate(double *loanAmount, float *interestRate, double *monthlyPayment, s
     return 0;
 }
 
-void output(std::string *calculatedOutput, int* months, double* totalInterest) {
+void output(std::string *calculatedOutput, int *months, double *totalInterest)
+{
     std::cout << "**********************************************************\n";
     std::cout << "\tAmortization Table\n";
-    std::cout << "Month\tBalance\tPayment\tRate\tInterest\tPrincipal\n";
+    std::cout << "Month\tBalance\t\tPayment\tRate\tInterest\tPrincipal\n";
     std::cout << "**********************************************************\n";
     std::cout << *calculatedOutput;
     std::cout << "**********************************************************\n";
     std::cout << "\n";
     std::cout << "It takes " << *months << " months to pay off the loan.\n";
-    std::cout << "Total interest paid is: $" << *totalInterest;
+    std::cout << "Total interest paid is: $" << format_precision_2(*totalInterest);
+}
+
+std::string format_precision_2(double number)
+{
+    // Create an output string stream
+    std::ostringstream streamObj;
+    // Set Fixed -Point Notation
+    streamObj << std::fixed;
+    // Set precision to 2 digits
+    streamObj << std::setprecision(2);
+    //Add double to stream
+    streamObj << number;
+    // Get string from output string stream
+    return streamObj.str();
+}
+
+std::string format_precision_2(float number)
+{
+    // Create an output string stream
+    std::ostringstream streamObj;
+    // Set Fixed -Point Notation
+    streamObj << std::fixed;
+    // Set precision to 2 digits
+    streamObj << std::setprecision(2);
+    //Add double to stream
+    streamObj << number;
+    // Get string from output string stream
+    return streamObj.str();
 }
