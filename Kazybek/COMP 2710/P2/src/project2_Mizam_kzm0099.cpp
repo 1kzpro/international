b718030e59a5d5ee35400580ba413727072d787d
1kzpro <kazymizam@mail.ru>
2020-09-14 10:10:36 -0500

    update

---
+++ Kazybek/COMP 2710/P2/src/project2_Mizam_kzm0099.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+#include <assert.h>
+#include <ctime>
+using namespace std;
+
+//prototypes
+bool at_least_two_alive(bool A_alive, bool B_alive, bool C_alive);
+/* Input: A_alive indicates whether Aaron is alive */
+/* B_alive indicates whether Bob is alive */
+/* C_alive indicates whether Charlie is alive */
+
+int main() {
+/*
+* This is the main function *...
+*/
+}
