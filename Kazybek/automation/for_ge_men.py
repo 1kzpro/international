41b692a8b992cdc8f28e08b003129f5a142e024e
1kzpro <kazymizam@mail.ru>
2020-09-26 09:22:13 -0500

    some updates

--- Kazybek/automation/for_ge_men.py
+++ Kazybek/automation/for_ge_men.py
@@ -1,15 +1,5 @@
-from selenium import webdriver
-import yaml
+import pyautogui
 
-conf = yaml.load(open('loginDetails.yml'))
-myFbEmail = conf['fb_user']['email']
-myFbPassword = conf['fb_user']['password']
-
-driver = webdriver.Chrome()
-
-def login(url,usernameId, username, passwordId, password, submit_buttonId):
-    driver.get(url)
-    driver.find_element_by_id(usernameId).send_keys(username)
-    driver.find_element_by_id(passwordId).send_keys(password)
-    driver.find_element_by_id(submit_buttonId).click()
-    driver.
+while True:
+    currentMouseX, currentMouseY = pyautogui.position()
+    print(currentMouseX, currentMouseY)
