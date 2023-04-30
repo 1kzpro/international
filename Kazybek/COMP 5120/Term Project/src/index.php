421eb306398302c507d2a31b59b53fbb75fee29e
1kzpro <kazymizam@mail.ru>
2023-04-30 17:45:18 -0500

    fix bug

--- Kazybek/COMP 5120/Term Project/src/index.php
+++ Kazybek/COMP 5120/Term Project/src/index.php
@@ -65,8 +65,6 @@
 
         if (array_key_exists('clearButton', $_POST)) {
             $sql_query_text = '';
-            $sql_query_result_html = '';
-            $error_msg = '';
             console_log("Clear Button Pressed!");
         }
     }
